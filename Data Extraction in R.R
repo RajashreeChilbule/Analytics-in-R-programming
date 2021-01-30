72489f556cfb6258f1f8ed5991f03548146d63e1
RajashreeChilbule <76646684+RajashreeChilbule@users.noreply.github.com>
2021-01-30 19:33:47 +0530

    Data extraction in R

--- Data Extraction in R.R
+++ Data Extraction in R.R
@@ -4,7 +4,7 @@
 # Exporting data
 
 # Export to Hard drive----
-#To begin with, you can save the data directly into the working directory. The following code prints the path of your working directory:
+#To begin with, you can save the data directly into the working directory. The following code prints the path of your working directory
 directory <-getwd()
 directory
 
@@ -17,7 +17,7 @@ data()  #other datasets
 #------------------Writing the dataset---------------------
 #Method 1:
 write.csv(mtcars, "mtcars2.csv")
-
+read.csv("mtcars2.csv")
 #Method 2:
 #make sure you have a folder : "data" in your project directory
 #like c1, c2, c3, c4, c5
@@ -29,7 +29,7 @@ write.csv(x=mtcars, file='data/mtcars2.csv', row.names = F)
 # Method 1:
 f<- file.choose()
 c1 <- read.csv(f)
-read.csv("mtcars2.csv")
+
 
 # Method 2:
 c_1 <- read.csv("data/mtcars1.csv")
